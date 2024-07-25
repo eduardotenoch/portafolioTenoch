fcb6870c2e62bbeed772f4505ad271d3c282e198
eduardotenoch <eduardotenoch2500@gmail.com>
2024-07-24 19:23:35 -0600

    routes cambio y diseño

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Routes } from './routes/Routes';
-import { Principal } from './components/Principal';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Routes />
-        <Principal />
-        <Footer />
-      </div>
+      <Header />
+      <Routes />
+      <Footer />
     </Router>
   );
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,14 @@
+// src/routes/Routes.js
 import React from 'react';
 import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import { Sobremi } from '../Pages/Sobremi';
+import { Principal } from '../components/Principal';
 import { Error404 } from '../Pages/Error404';
 
 export function Routes() {
   return (
     <RouterRoutes>
-      <Route path="/" element={<Sobremi />} />
+      <Route path="portafolioTenoch" element={<Principal />} />
       <Route path="/sobremi" element={<Sobremi />} />
       <Route path="*" element={<Error404 />} />
     </RouterRoutes>
